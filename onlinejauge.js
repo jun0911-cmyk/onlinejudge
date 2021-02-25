880b851d90d93ae986a2ad07c4cf369d6bb90166
jun0911-cmyk <jun.0911@daum.net>
2021-02-25 20:59:13 +0900

    onlinejauge

--- onlinejauge.js
+++ onlinejauge.js
@@ -2,6 +2,25 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host : '127.0.0.1',
+    user : 'root',
+    password : '1234',
+    database : 'online_jauge',
+    charset : 'utf8'
+});
+
+connection.connect();
+
+// 온라인 저지 데이터베이스 연결
+connection.query('SELECT * from jauge_table', (err, rows, fields) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('mysql 온라인저지 서버데이터베이스 연결 완료');
+    }
+});
 
 const app = express();
 app.use(express.static(path.join(__dirname,'/')));
