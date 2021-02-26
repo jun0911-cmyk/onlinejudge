0b6f39791f48750148c08b6f949f3c5619a9c366
jun0911-cmyk <jun.0911@daum.net>
2021-02-26 12:48:57 +0900

    onlinejauge

--- onlinejauge.js
+++ onlinejauge.js
@@ -18,7 +18,7 @@ connection.query('SELECT * from jauge_table', (err, rows, fields) => {
     if(err) {
         console.log(err);
     } else {
-        console.log('mysql 온라인저지 서버데이터베이스 연결 완료');
+        console.log('mysql 온라인저지 서버 데이터베이스 연결 완료');
     }
 });
 
@@ -28,18 +28,36 @@ app.use(morgan('dev'));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
-app.get('/code', (req, res) => {
+// 파일 받아옴
+app.get('/onlinejauge', (req, res) => {
     res.sendFile(path.join(__dirname, 'tset.html'))
 });
 
-app.post('/code', (req, res, next) => {
-    var server = req.body.server; 
+// post 연결
+app.post('/onlinejauge', (req, res, next) => {
+    // 코드를 가져옴
+    var server = req.body.server;
+    
+    // 컴파일 도구 선택
+    const jaugeserver = function(comfile) {
+        switch(comfile) {
+            case 'c':
+                return 'c_cpp';
+            case 'cpp':
+                return 'c_cpp';
+            default:
+                return 'c_cpp';
+        }
+    }
+    console.log(jaugeserver);
     console.log('코드 입력 \n' + server);
-    res.status(302).redirect('/code');
-    console.log('서버전송성공');
+
+    // 성공 시 /onlinejauge로 되돌림
+    res.status(302).redirect('/onlinejauge');
+    console.log('서버로 전송을 성공하였습니다');
 });
 
 // 포트연결
 app.listen(3000, function() {
-    console.log('3000');
+    console.log('3000번포트 테스트 온라인저지 서버 구동성공');
 });
