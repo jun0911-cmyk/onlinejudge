ad361e8be50bbf7f275e8bbea93c2288a52287ed
jun0911 <71050600+jun0911-cmyk@users.noreply.github.com>
2021-02-27 13:46:27 +0900

    onlinejauge

--- shell.js
+++ shell.js
@@ -2,7 +2,9 @@ const shell = require('shelljs');
 
 shell.cd('~');
 process.stdin.resume();
+// utf8로 인코딩
 process.stdin.setEncoding('utf8');
+// 표준입력처리 
 process.stdin.on('data', function (data) {
     process.stdout.write(JSON.stringify(JSON.parse(data), null, 2))
 })
