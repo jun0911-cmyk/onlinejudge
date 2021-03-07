b9b2fdbd40a5abd94ab0563c5a3937bdfc059df5
jun0911 <71050600+jun0911-cmyk@users.noreply.github.com>
2021-03-07 22:24:08 +0900

    onlinejauge

--- gitpush.js
+++
@@ -1,9 +0,0 @@
-const shell = require("shelljs");
-
-console.log("github 디랙토리로 서버 올립니다");
-shell.exec('git add .');
-console.log('파일 선택 성공');
-shell.exec('git commit -m "onlinejauge"');
-console.log('전송준비 완료');
-shell.exec('git push origin master');
-console.log('업로드 완료');
